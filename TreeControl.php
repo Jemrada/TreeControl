276d7bd7b4e0dba850cf741d33163b47ee2470fe
Jemrada <jemrada@me.com>
2013-11-24 11:56:18 -0600

    Update TreeControl.php

--- TreeControl.php
+++ TreeControl.php
@@ -3,27 +3,32 @@
 /*
 __PocketMine Plugin__
 name=TreeControl
-description=Simple to Advanced Tree Control
-version=0.1
+description=Simple to Advanced Tree Control.
+version=1.0
 author=Jemrada
-class=TreeControl
-apiversion=9,10
+class=TreeLife
+apiversion=10
 */
 
-class TreeControl implements Plugin{
-   private $api;
+    class TreeLife implements plugin{
 
-   public function __construct(ServerAPI $api, $server = false){
-     $this->api = $api;
-   }
-   
-   public function init(){
-     $this->api->console->register("SaplingMature", "Command for growing a tree", array($this, "command"));
-            $this,
-            "SaplingMature"
-   }
-   
-   public function __destruct(){
-   }
+    private $api;
 
+    public function __construct(ServerAPI $api, $server = false){
+
+		$this->api = $api;
+
+	}
+
+	public function init(){
+
+    $this->api->addHandler("player.block.break", array($this, "eventHandler"), 100);
+
+    }
+    
+    public function eventHandler($data, $event)
+    {
+    switch($event)
+    {
+        case 'player.block.break':
 ?>
